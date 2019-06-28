fad6fe91a8b1ce6e8e92689f5efd6265ae0641b3
uk98 <kumar.utkarsh0611@gmail.com>
2019-06-29 00:58:58 +0530

    Made a nodejs frontend with Express and EJS view engine

---
+++ Project/contracts/SimpleStorage.sol
@@ -0,0 +1,20 @@
+// File: `./contracts/SimpleStorage.sol`
+
+pragma solidity ^0.5.8;
+
+contract SimpleStorage {
+  uint public storedData;
+
+  constructor(uint initVal) public {
+    storedData = initVal;
+  }
+
+  function set(uint x) public {
+    storedData = x;
+  }
+
+  function get() view public returns (uint retVal) {
+    return storedData;
+  }
+}
+
---
+++ node-website/routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {page:'Home', menuId:'home'});
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', {page:'About Us', menuId:'about'});
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {page:'Contact Us', menuId:'contact'});
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', {page:'Login', menuId:'login'});
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup', {page:'Signup', menuId:'signup'});
+});
+module.exports = router;
